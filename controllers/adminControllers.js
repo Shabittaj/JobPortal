088129e65306f16c1226be9d5e744fbc9e58ffc5
Shabit Taj S <shabittajsameer1920@gmail.com>
2024-02-21 13:04:33 +0530

    updated

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -2,14 +2,20 @@ import jobModel from '../models/jobModel.js';
 import jobSeekerModel from '../models/jobSeekerModel.js';
 import employerModel from '../models/employerModel.js';
 import applicationModel from '../models/applicationModel.js';
+import userModel from '../models/userModel.js';
 
 export const dashboard = async (req, res, next) => {
     try {
-        const totalJobs = await jobModel.countDocuments();
-        const totalEmployers = await employerModel.countDocuments();
-        const totalJobSeekers = await jobSeekerModel.countDocuments();
-        const totalApplications = await applicationModel.countDocuments();
-        res.status(200).json({ totalJobs, totalEmployers, totalJobSeekers, totalApplications });
+        if (req.user.role === 'admin') {
+            const totalAdmins = await userModel.find({ role: 'admin' }).countDocuments();
+            const totalJobs = await jobModel.countDocuments();
+            const totalEmployers = await employerModel.countDocuments();
+            const totalJobSeekers = await jobSeekerModel.countDocuments();
+            const totalApplications = await applicationModel.countDocuments();
+            res.status(200).json({ totalAdmins, totalJobs, totalEmployers, totalJobSeekers, totalApplications });
+        } else {
+            next('only admin have the access to this route');
+        }
     } catch (error) {
         next(error)
     }
